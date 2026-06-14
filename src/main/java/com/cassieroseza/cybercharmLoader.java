1a332c9b77069eeb164a8c54a85607f38f70ea5b
CassieRoseZA <13422754+CassieRoseZA@users.noreply.github.com>
2026-06-14 21:07:08 +0200

    Initial Commit

---
+++ src/main/java/com/cassieroseza/cybercharmLoader.java
@@ -0,0 +1,12 @@
+package com.cassieroseza;
+
+import io.papermc.paper.plugin.loader.PluginClasspathBuilder;
+import io.papermc.paper.plugin.loader.PluginLoader;
+
+class cybercharmLoader implements PluginLoader {
+
+    @Override
+    public void classloader(final PluginClasspathBuilder builder) {
+        // Add dynamically loaded libraries here
+    }
+}
